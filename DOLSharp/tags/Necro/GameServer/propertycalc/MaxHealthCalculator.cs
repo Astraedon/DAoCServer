1532dfd5b629abd4adc0ac8cb09ab6f2848d29d8
aredhel77 <aredhel77@c9877f0d-d2ea-46ae-93cb-121acdf3e971>
2007-10-26 16:54:20 +0000

    made a copy

---
+++ DOLSharp/tags/Necro/GameServer/propertycalc/MaxHealthCalculator.cs
@@ -0,0 +1,91 @@
+using System;
+
+using DOL.GS.Keeps;
+
+namespace DOL.GS.PropertyCalc
+{
+	/// <summary>
+	/// The Max HP calculator
+	///
+	/// BuffBonusCategory1 is used for absolute HP buffs
+	/// BuffBonusCategory2 unused
+	/// BuffBonusCategory3 unused
+	/// BuffBonusCategory4 unused
+	/// BuffBonusMultCategory1 unused
+	/// </summary>
+	[PropertyCalculator(eProperty.MaxHealth)]
+	public class MaxHealthCalculator : PropertyCalculator
+	{
+		public override int CalcValue(GameLiving living, eProperty property)
+		{
+			if (living is GamePlayer)
+			{
+				GamePlayer player = living as GamePlayer;
+				int hpBase = player.CalculateMaxHealth(player.Level, player.GetModified(eProperty.Constitution));
+				int buffBonus = living.BuffBonusCategory1[(int)property];
+				if (buffBonus < 0) buffBonus = (int)((1 + (buffBonus / -100.0)) * hpBase)-hpBase;
+				int itemBonus = living.ItemBonus[(int)property];
+				int cap = Math.Max(player.Level * 4, 20) + // at least 20
+						  Math.Min(living.ItemBonus[(int)eProperty.MaxHealthCapBonus], player.Level * 4);	
+				itemBonus = Math.Min(itemBonus, cap);
+                if (player.HasAbility(Abilities.ScarsOfBattle) && player.Level >= 40)
+                {
+                    int levelbonus = Math.Min(player.Level - 40, 10);
+                    hpBase = (int)(hpBase * (100 + levelbonus) * 0.01);
+                }
+				int abilityBonus = living.AbilityBonus[(int)property];
+
+				return Math.Max(hpBase + itemBonus + buffBonus + abilityBonus, 1); // at least 1
+			}
+			else if ( living is GameKeepComponent )
+			{
+				GameKeepComponent keepcomp = living as GameKeepComponent;
+				return (keepcomp.Keep.EffectiveLevel(keepcomp.Keep.Level) + 1) * keepcomp.Keep.BaseLevel * 200;
+			}
+			else if ( living is GameKeepDoor )
+			{
+				GameKeepDoor keepdoor = living as GameKeepDoor;
+				return (keepdoor.Component.Keep.EffectiveLevel(keepdoor.Component.Keep.Level) + 1) * keepdoor.Component.Keep.BaseLevel * 200;
+				//todo : use material too to calculate maxhealth
+			}
+			else if (living is GameNPC)
+			{
+				if (living.Level<10)
+				{
+					return living.Level * 20 + 20 + living.BuffBonusCategory1[(int)property];	// default
+				}
+				else
+				{
+					// approx to original formula, thx to mathematica :)
+					int hp = (int)(50 + 11*living.Level + 0.548331 * living.Level * living.Level) + living.BuffBonusCategory1[(int)property];
+					if (living.Level < 25)
+					{
+						hp += 20;
+					}
+                    //Fix for theurg and animist pets
+                    if (((GameNPC)living).HealthMultiplicator)
+                        return hp / 2;
+                    else
+					    return hp;
+				}
+			}
+            else
+            {
+                if (living.Level < 10)
+                {
+                    return living.Level * 20 + 20 + living.BuffBonusCategory1[(int)property];	// default
+                }
+                else
+                {
+                    // approx to original formula, thx to mathematica :)
+                    int hp = (int)(50 + 11 * living.Level + 0.548331 * living.Level * living.Level) + living.BuffBonusCategory1[(int)property];
+                    if (living.Level < 25)
+                    {
+                        hp += 20;
+                    }
+                    return hp;
+                }
+            }
+		}
+	}
+}
---
+++ DOLSharp/tags/Necro/GameServer/spells/PropertyChangingSpell.cs
@@ -0,0 +1,351 @@
+/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+using System.Reflection;
+
+using DOL.Database;
+using DOL.GS.Effects;
+using DOL.GS.PacketHandler;
+using DOL.GS.PropertyCalc;
+
+using log4net;
+
+namespace DOL.GS.Spells
+{
+	/// <summary>
+	/// Spell to change up to 3 property bonuses at once
+	/// in one their specific given bonus category
+	/// </summary>
+	public abstract class PropertyChangingSpell : SpellHandler
+	{
+		/// <summary>
+		/// Execute property changing spell
+		/// </summary>
+		/// <param name="target"></param>
+		public override void FinishSpellCast(GameLiving target)
+		{
+			m_caster.Mana -= CalculateNeededPower(target);
+			base.FinishSpellCast(target);
+		}
+		public override void ApplyEffectOnTarget(GameLiving target, double effectiveness)
+		{
+			// vampiir, they cannot be buffed except with resists/armor factor/ haste / power regen 
+			GamePlayer vampiir = target as GamePlayer;
+			if (vampiir != null && HasPositiveEffect && vampiir.CharacterClass.ID == (int)eCharacterClass.Vampiir && m_caster != vampiir)
+			{
+				//restrictions
+				if (this is PropertyChangingSpell
+					&& this is ArmorFactorBuff == false
+					&& this is CombatSpeedBuff == false
+                    && this is AbstractResistBuff == false
+                    && this is EnduranceRegenSpellHandler == false
+                    && this is EvadeChanceBuff == false
+                    && this is ParryChanceBuff == false)
+				{
+					GamePlayer caster = m_caster as GamePlayer;
+					if (caster != null)
+					{
+						caster.Out.SendMessage("Your buff has no effect on the Vampiir!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+					}
+					vampiir.Out.SendMessage("This buff has no effect on you!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+					return;
+				}
+			}
+
+            if (vampiir != null && this is HeatColdMatterBuff)
+            {
+                GameSpellEffect Slash = FindEffectOnTarget(vampiir, "MatterResistBuff");
+                GameSpellEffect Crush = FindEffectOnTarget(vampiir, "ColdResistBuff");
+                GameSpellEffect Thrust = FindEffectOnTarget(vampiir, "HeatResistBuff");
+                if (Slash != null || Crush != null || Thrust != null)
+                {
+                    MessageToCaster(target.Name + " already has this effect", eChatType.CT_SpellResisted);
+                    return;
+                }
+            }
+
+            if (vampiir != null && this is BodySpiritEnergyBuff)
+            {
+                GameSpellEffect Body = FindEffectOnTarget(vampiir, "BodyResistBuff");
+                GameSpellEffect Spirit = FindEffectOnTarget(vampiir, "SpiritResistBuff");
+                GameSpellEffect Energy = FindEffectOnTarget(vampiir, "EnergyResistBuff");
+                if (Body != null || Spirit != null || Energy != null)
+                {
+                    MessageToCaster(target.Name + " already has this effect", eChatType.CT_SpellResisted);
+                    return;
+                }
+            }
+			base.ApplyEffectOnTarget(target, effectiveness);
+		}
+		/// <summary>
+		/// start changing effect on target
+		/// </summary>
+		/// <param name="effect"></param>
+		public override void OnEffectStart(GameSpellEffect effect)
+		{
+			IPropertyIndexer bonuscat = GetBonusCategory(effect.Owner, BonusCategory1);
+
+			int amount = (int)(Spell.Value * effect.Effectiveness);
+
+			bonuscat[(int)Property1] += amount;
+
+			if (Property2 != eProperty.Undefined)
+			{
+				bonuscat = GetBonusCategory(effect.Owner, BonusCategory2);
+				bonuscat[(int)Property2] += amount;
+			}
+			if (Property3 != eProperty.Undefined)
+			{
+				bonuscat = GetBonusCategory(effect.Owner, BonusCategory3);
+				bonuscat[(int)Property3] += amount;
+			}
+
+			SendUpdates(effect.Owner);
+
+			eChatType toLiving = eChatType.CT_SpellPulse;
+			eChatType toOther = eChatType.CT_SpellPulse;
+			if (Spell.Pulse == 0 || !HasPositiveEffect)
+			{
+				toLiving = eChatType.CT_Spell;
+				toOther = eChatType.CT_System;
+				SendEffectAnimation(effect.Owner, 0, false, 1);
+			}
+
+			//messages are after buff and after "Your xxx has increased." messages
+			MessageToLiving(effect.Owner, Spell.Message1, toLiving);
+			Message.SystemToArea(effect.Owner, Util.MakeSentence(Spell.Message2, effect.Owner.GetName(0, false)), toOther, effect.Owner);
+
+			if (ServerProperties.Properties.BUFF_RANGE > 0 && effect.Spell.Concentration > 0 && effect.SpellHandler.HasPositiveEffect && effect.Owner != effect.SpellHandler.Caster)
+			{
+				m_buffCheckAction = new BuffCheckAction(effect.SpellHandler.Caster, effect.Owner, effect);
+				m_buffCheckAction.Start(BuffCheckAction.BUFFCHECKINTERVAL);
+			}
+		}
+
+		BuffCheckAction m_buffCheckAction = null;
+
+		/// <summary>
+		/// When an applied effect expires.
+		/// Duration spells only.
+		/// </summary>
+		/// <param name="effect">The expired effect</param>
+		/// <param name="noMessages">true, when no messages should be sent to player and surrounding</param>
+		/// <returns>immunity duration in milliseconds</returns>
+		public override int OnEffectExpires(GameSpellEffect effect, bool noMessages)
+		{
+			if (!noMessages && Spell.Pulse == 0)
+			{
+				MessageToLiving(effect.Owner, Spell.Message3, eChatType.CT_SpellExpires);
+				Message.SystemToArea(effect.Owner, Util.MakeSentence(Spell.Message4, effect.Owner.GetName(0, false)), eChatType.CT_SpellExpires, effect.Owner);
+			}
+
+			IPropertyIndexer bonuscat = GetBonusCategory(effect.Owner, BonusCategory1);
+			bonuscat[(int) Property1] -= (int) (Spell.Value*effect.Effectiveness);
+
+			if (Property2 != eProperty.Undefined)
+			{
+				bonuscat = GetBonusCategory(effect.Owner, BonusCategory2);
+				bonuscat[(int) Property2] -= (int) (Spell.Value*effect.Effectiveness);
+			}
+			if (Property3 != eProperty.Undefined)
+			{
+				bonuscat = GetBonusCategory(effect.Owner, BonusCategory3);
+				bonuscat[(int) Property3] -= (int) (Spell.Value*effect.Effectiveness);
+			}
+
+			SendUpdates(effect.Owner);
+
+			if (m_buffCheckAction != null)
+			{
+				m_buffCheckAction.Stop();
+				m_buffCheckAction = null;
+			}
+
+			return base.OnEffectExpires(effect, noMessages);
+		}
+
+		protected virtual void SendUpdates(GameLiving target)
+		{
+		}
+
+		protected IPropertyIndexer GetBonusCategory(GameLiving target, int categoryid)
+		{
+			IPropertyIndexer bonuscat = null;
+			switch (categoryid)
+			{
+				case 1:
+					bonuscat = target.BuffBonusCategory1;
+					break;
+				case 2:
+					bonuscat = target.BuffBonusCategory2;
+					break;
+				case 3:
+					bonuscat = target.BuffBonusCategory3;
+					break;
+				case 4:
+					bonuscat = target.BuffBonusCategory4;
+					break;
+				default:
+					if (log.IsErrorEnabled)
+						log.Error("BonusCategory not found " + categoryid + "!");
+					break;
+			}
+			return bonuscat;
+		}
+
+		/// <summary>
+		/// Property 1 which bonus value has to be changed
+		/// </summary>
+		public abstract eProperty Property1 { get; }
+
+		/// <summary>
+		/// Property 2 which bonus value has to be changed
+		/// </summary>
+		public virtual eProperty Property2
+		{
+			get { return eProperty.Undefined; }
+		}
+
+		/// <summary>
+		/// Property 3 which bonus value has to be changed
+		/// </summary>
+		public virtual eProperty Property3
+		{
+			get { return eProperty.Undefined; }
+		}
+
+		/// <summary>
+		/// Bonus Category where to change the Property1
+		/// </summary>
+		public virtual int BonusCategory1
+		{
+			get { return 1; }
+		}
+
+		/// <summary>
+		/// Bonus Category where to change the Property2
+		/// </summary>
+		public virtual int BonusCategory2
+		{
+			get { return 1; }
+		}
+
+		/// <summary>
+		/// Bonus Category where to change the Property3
+		/// </summary>
+		public virtual int BonusCategory3
+		{
+			get { return 1; }
+		}
+
+		public override void OnEffectRestored(GameSpellEffect effect, int[] vars)
+		{
+			IPropertyIndexer bonuscat = GetBonusCategory(effect.Owner, BonusCategory1);
+			bonuscat[(int)Property1] += vars[1];
+
+			if (Property2 != eProperty.Undefined)
+			{
+				bonuscat = GetBonusCategory(effect.Owner, BonusCategory2);
+				bonuscat[(int)Property2] += vars[1];
+			}
+			if (Property3 != eProperty.Undefined)
+			{
+				bonuscat = GetBonusCategory(effect.Owner, BonusCategory3);
+				bonuscat[(int)Property3] += vars[1];
+			}
+			SendUpdates(effect.Owner);
+		}
+
+		public override int OnRestoredEffectExpires(GameSpellEffect effect, int[] vars, bool noMessages)
+		{
+			if (!noMessages && Spell.Pulse == 0)
+			{
+				MessageToLiving(effect.Owner, Spell.Message3, eChatType.CT_SpellExpires);
+				Message.SystemToArea(effect.Owner, Util.MakeSentence(Spell.Message4, effect.Owner.GetName(0, false)), eChatType.CT_SpellExpires, effect.Owner);
+			}
+
+			IPropertyIndexer bonuscat = GetBonusCategory(effect.Owner, BonusCategory1);
+			bonuscat[(int)Property1] -= vars[1];
+
+			if (Property2 != eProperty.Undefined)
+			{
+				bonuscat = GetBonusCategory(effect.Owner, BonusCategory2);
+				bonuscat[(int)Property2] -= vars[1];
+			}
+			if (Property3 != eProperty.Undefined)
+			{
+				bonuscat = GetBonusCategory(effect.Owner, BonusCategory3);
+				bonuscat[(int)Property3] -= vars[1];
+			}
+			SendUpdates(effect.Owner);
+
+			return 0;
+
+		}
+
+		public override PlayerXEffect getSavedEffect(GameSpellEffect e)
+		{
+			PlayerXEffect eff = new PlayerXEffect();
+			eff.Var1 = Spell.ID;
+			eff.Duration = e.RemainingTime;
+			eff.IsHandler = true;
+			eff.Var2 = (int)(Spell.Value * e.Effectiveness);
+			eff.SpellLine = SpellLine.KeyName;
+			return eff;
+
+		}
+
+		// constructor
+		public PropertyChangingSpell(GameLiving caster, Spell spell, SpellLine line) : base(caster, spell, line)
+		{
+		}
+	}
+
+	public class BuffCheckAction : RegionAction 
+	{
+		public const int BUFFCHECKINTERVAL = 60000;//60 seconds
+
+		private GameLiving m_caster = null;
+		private GameLiving m_owner = null;
+		private GameSpellEffect m_effect = null;
+
+		public BuffCheckAction(GameLiving caster, GameLiving owner, GameSpellEffect effect)
+			: base(caster)
+		{
+			m_caster = caster;
+			m_owner = owner;
+			m_effect = effect;
+		}
+
+		/// <summary>
+		/// Called on every timer tick
+		/// </summary>
+		protected override void OnTick()
+		{
+			if (m_caster == null ||
+				m_owner == null ||
+				m_effect == null)
+				return;
+
+			if (WorldMgr.GetDistance(m_caster, m_owner) > ServerProperties.Properties.BUFF_RANGE)
+				m_effect.Cancel(false);
+			else
+				Start(BUFFCHECKINTERVAL);
+		}
+	}
+}
